547b23263c76a7b3b8f33f64fccb2f4ccb114e74
Radoslav Draganov <radoslav.draganov.a55@learn.telerikacademy.com>
2024-01-18 01:01:45 +0200

    package.json + update to constants with API_KEY, trending, search and gif details urls

--- template/src/common/constants.js
+++ template/src/common/constants.js
@@ -11,3 +11,11 @@ export const CONTAINER_SELECTOR = '#container';
 export const FULL_HEART = '❤';
 
 export const EMPTY_HEART = '♡';
+
+export const API_KEY = 'GB1eH419EgtMMzttQ7t1wOClKAcArPG4';
+
+export const trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=25&offset=0&rating=g`;
+
+export const searchURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=&limit=25&offset=0&rating=g`;
+
+export const displayGifDetailsURL = `https://api.giphy.com/v1/gifs/?api_key=${API_KEY}&rating=g`;
