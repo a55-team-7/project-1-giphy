5a48e86bad3e6e357d5c1ec22f98ea96c0c58fd7
Radoslav Draganov <radoslav.draganov.a55@learn.telerikacademy.com>
2024-01-19 05:04:01 +0200

    minor import fix

--- template/src/events/navigation-events.js
+++ template/src/events/navigation-events.js
@@ -1,5 +1,5 @@
 import { ABOUT, CATEGORIES, CONTAINER_SELECTOR, FAVORITES, HOME, TRENDING } from '../common/constants.js';
-import { loadCategories, loadCategory, loadMovies, loadSingleMovie } from '../requests/request-service.js';
+import { loadCategories, loadCategory, loadMovies, loadSingleMovie, loadTrendingGifs } from '../requests/request-service.js';
 import { toAboutView } from '../views/about-view.js';
 import { toCategoriesView } from '../views/category-view.js';
 import { toFavoritesView } from '../views/favorites-view.js';
