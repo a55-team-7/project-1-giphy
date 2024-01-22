4d70141c2d5c8a85a1028b172d1405810373bec5
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-01-22 15:01:44 +0200

    Try fix the favourites view

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -11,7 +11,7 @@ export const toFavoritesView = (favoriteGifs, randomGif) => `
   <h1>You can see your favourites here! </h1>
   <div class="content">
   <p>Total liked: ${favoriteGifs.length}</p>
-    ${favoriteGifs.map(toGifSimple).join('\n') || `<p>Like some gifs to see them here.</p><p>Loading random:</p>${toGifSimple(randomGif)}`}
+    ${favoriteGifs.map(toGifSimple) || `<p>Like some gifs to see them here.</p><p>Loading random:</p>${randomGif.map(toGifSimple)}`}
   </div>
 </div>
 `;
