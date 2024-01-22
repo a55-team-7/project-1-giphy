1ba0b2f24bf0f898e509a9deee91e5d26ad1c476
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-01-22 23:27:55 +0200

    Implement function which chooses between favorite or random

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -11,9 +11,22 @@ export const toFavoritesView = (favoriteGifs, randomGif) => `
   <h1>You can see your favourites here! </h1>
   <div class="content">
   <p>Total liked: ${favoriteGifs.length}</p>
-  ${toGifSimple(randomGif)}
+  ${chooseRandomOrFavourite(favoriteGifs, randomGif)}
   </div>
 </div>
 `;
 
-//favourite or randon? function 
+/**
+ * Returns an array of simplified GIF objects if there are favorite GIFs,
+ * otherwise returns a single simplified GIF object.
+ * @param {Array} favoriteGifs - The array of favorite GIF objects.
+ * @param {Object} randomGif - The random GIF object.
+ * @returns {(Array|Object)} - An array of simplified GIF objects or a single simplified GIF object.
+ */
+const chooseRandomOrFavourite = (favoriteGifs, randomGif) => {
+  if (favoriteGifs.length > 0) {
+    return favoriteGifs.map(toGifSimple);
+  } else {
+    return toGifSimple(randomGif);
+  }
+}
