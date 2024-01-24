cd3ec56a3cda43dd482fb9e5b4b07f4f91d6796a
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-01-24 14:16:58 +0200

    Finish about

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -8,6 +8,7 @@ export const toAboutView = () => `
     <h1>About the app</h1> 
     <h2>Authors: Telerik Academy Students: Vyara, Rado and Yordan</h2>  
     <h2>Date: January 2023</h2>
+    <img src=https://media3.giphy.com/media/FPDZV2JGkNGeUZdi7G/200w.gif?cid=5d1c6089e9afudzcod01zpv25kztbkeystiwb8c7o7eeu1xl&ep=v1_gifs_gifId&rid=200w.gif&ct=g alt="gif">
   </div>
 </div>
 `;
