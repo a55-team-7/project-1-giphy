9ff2ce18c4d2a6534339f0090a1079e914182739
Yordan <yordan.blagov@abv.bg>
2024-01-24 14:18:58 +0200

    about-view date

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -7,7 +7,7 @@ export const toAboutView = () => `
   <div class="content">
     <h1>About the app</h1> 
     <h2>Authors: Telerik Academy Students: Vyara, Rado and Yordan</h2>  
-    <h2>Date: January 2023</h2>
+    <h2>Date: January 2024</h2>
     <img src=https://media3.giphy.com/media/FPDZV2JGkNGeUZdi7G/200w.gif?cid=5d1c6089e9afudzcod01zpv25kztbkeystiwb8c7o7eeu1xl&ep=v1_gifs_gifId&rid=200w.gif&ct=g alt="gif">
   </div>
 </div>
