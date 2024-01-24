08d5cb848312b076b7e104d57c18e13bd877f4bb
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-01-24 14:03:37 +0200

    Fix about view

--- template/src/views/about-view.js
+++ template/src/views/about-view.js
@@ -5,8 +5,8 @@
 export const toAboutView = () => `
 <div id="about">
   <div class="content">
-    <h1>About the app</h1>
-    <h2>Authors: Telerik Academy Students: Vyara, Rado and Yordan</h2>
+    <h1>About the app</h1> 
+    <h2>Authors: Telerik Academy Students: Vyara, Rado and Yordan</h2>  
     <h2>Date: January 2023</h2>
   </div>
 </div>
